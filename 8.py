fe2290f16204ee490f5920d4f400780592df8996
Ganesh Raj <ganeshbr1125@gmail.com>
2026-07-09 22:37:34 +0530

    Delete 8.py

--- 8.py
+++
@@ -1,9 +0,0 @@
-karnataka_food={
-    "Bengaluru":"Bisi Bele Bath",
-    "Mysuru":"Mysore pak",
-    "Mangaluru":"Neer Dosa"
-}
-
-new_dishes={"Huballi":"Girmit"}
-karnataka_food.update(new_dishes)
-print(karnataka_food)
